53ac39c42d4f1dcc443e07f0285434e9796306ae
297975-soumya <Soumyaranjan.Dikshit@ust.com>
2025-08-04 20:51:39 +0530

    Uploaded dictionary.py

--- assessment_exercises.py
+++ assessment_exercises.py
@@ -154,23 +154,23 @@
 # Input: {"x": 10, "y": {"a": 5, "b": {"z": 1}}}
 # Output: {'x': 10, 'y.a': 5, 'y.b.z': 1}
 
-input_dict = {"x": 10, "y": {"a": 5, "b": {"z": 1}}}
-flattened_dict = {}
-recursion_tracker = [(input_dict, "")]
-
-while recursion_tracker:
-    current_dict, key_prefix = recursion_tracker.pop()
-    for key, value in current_dict.items():
-        if key_prefix:
-            new_key = f"{key_prefix}.{key}"
-        else:
-            new_key = key
-        if isinstance(value, dict):
-            recursion_tracker.append((value, new_key))
-        else:
-            flattened_dict[new_key] = value
-
-print(flattened_dict)
+# input_dict = {"x": 10, "y": {"a": 5, "b": {"z": 1}}}
+# flattened_dict = {}
+# recursion_tracker = [(input_dict, "")]
+#
+# while recursion_tracker:
+#     current_dict, key_prefix = recursion_tracker.pop()
+#     for key, value in current_dict.items():
+#         if key_prefix:
+#             new_key = f"{key_prefix}.{key}"
+#         else:
+#             new_key = key
+#         if isinstance(value, dict):
+#             recursion_tracker.append((value, new_key))
+#         else:
+#             flattened_dict[new_key] = value
+#
+# print(flattened_dict)
 
 # 5. Hourglass Star Pattern:
 # Input: 5
@@ -185,14 +185,182 @@ print(flattened_dict)
 #  *******
 # *********
 
-n1 = 5
-for i in range(1, n1 + 1):
-    print(" "*i+"*"*((n1*2)-1))
-    n1-=1
-n2 = 5
-for j in range(2, n2 + 1):
-    print(" "*(n2-1)+"*"*((j*2)-1))
-    n2-=1
+# n1 = 5
+# for i in range(1, n1 + 1):
+#     print(" "*i+"*"*((n1*2)-1))
+#     n1-=1
+# n2 = 5
+# for j in range(2, n2 + 1):
+#     print(" "*(n2-1)+"*"*((j*2)-1))
+#     n2-=1
+
+
+# __SET 3__:
+
+# 1. Add two Floats and Print Boolean:
+# Input two float values. Print True if their sum > 100, else False.
+# float1 = input("Enter first float value: ")
+# float2 = input("Enter second float value: ")
+# print(float(float1) + float(float2) > 100)
+
+# 2. Reverse List manually:
+# Input a list of integers and reverse it manually (no slicing).
+# input_list = [1, 2, 3]
+# reversed_list = []
+# for i in input_list:
+#     reversed_list.insert(0,i)
+# print(reversed_list)
+
+# 3. Leap year checker:
+# A year is leap if:
+#    - Div by 4 and not by 100 or
+#    - Div by 400
+# year = input("Enter a year: ")
+# if year.isdigit():
+#     year = int(year)
+#     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#         print(f"{year} is a leap year.")
+#     else:
+#         print(f"{year} is not a leap year.")
+# else:
+#     print("Please enter a valid year in numeric format.")
+
+# 4. Flatten List of Dicts to Single Dict:
+# Given: [{'a': 1}, {'b': 2, 'c': 3}, {'d': {'e': 4}}]
+# Output: {'a': 1, 'b': 2, 'c': 3, 'd.e': 4}
+# dict_list = [{'a': 1}, {'b': 2, 'c': 3}, {'d': {'e': 4}}]
+# flattened = {}
+# for d in dict_list:
+#     tracker = [(d, "")]
+#     while tracker:
+#         current_d, key_prefix = tracker.pop()
+#         for key, value in current_d.items():
+#             if key_prefix:
+#                 new_key = f"{key_prefix}.{key}"
+#             else:
+#                 new_key = key
+#             if isinstance(value, dict):
+#                 tracker.append((value, new_key))
+#             else:
+#                 flattened[new_key] = value
+# print(flattened)
+
+# 5. Diamond Star Pattern:
+# Input: 5
+# Output:
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+# rows = input("Enter a positive number: ")
+# if rows.isdigit():
+#     rows = int(rows)
+# rows1 = int(rows) - 1
+# for i in range(1, rows+1):
+#     print(" "*rows + "*"*(i*2 - 1))
+#     rows-=1
+# for j in range(1, rows1+1):
+#     print(" "*(j+1) + "*"*(rows1*2 - 1))
+#     rows1-=1
+
+
+# __SET 4__:
+
+# 1. Simple Calculator using Operators:
+# Input two integers and an operator (+,-,*,/). Perform operation.
+# num1 = input("Enter first number: ")
+# num2 = input("Enter second number: ")
+# if num1.isdigit() and num2.isdigit():
+#     num1 = int(num1)
+#     num2 = int(num2)
+# else:
+#     print("Invalid input")
+#     exit()
+# op = input("Enter operator (+, -, *, /): ")
+# result = 0
+# if op == "+":
+#     result = num1 + num2
+# elif op == "-":
+#     result = num1 - num2
+# elif op == "*":
+#     result = num1 * num2
+# elif op == "/":
+#     result = num1 / num2
+# else:
+#     print("Invalid Operator")
+#     exit()
+# print(f"{num1} {op} {num2} = {result}")
+
+# 2. Float List: Avg>60 Check
+# Input N floats. Calculate average and check if>60. Print True/False.
+# input_string = input("Enter multiple float values with spaces: ")
+# float_list = []
+# for i in input_string.split():
+#     float_list.append(float(i))
+#
+# average = sum(float_list) / len(float_list)
+#
+# print(average>60)
+
+# 3. Login attempt limit with while loop:
+# Give user 3 attempts to enter correct password. Lock account after 3 login attempts.
+# password = "Admin@123"
+# count = 3
+# while count!=0:
+#     pass_input = input("Username: Admin \nEnter your password: ")
+#     if pass_input == password:
+#         print("Login Successful!")
+#         exit()
+#     else:
+#         count-=1
+#         print(f"Invalid Password \nYou have {count} attempts left.")
+# if count==0:
+#     print("Account Locked!!")
+
+# 4. Recursive Dictionary Flattener (Full)
+# Input deeply nested dictionary and flatten all keys.
+# # Example: {"a": {"b": {"c": 1}}, "x": 2} -> {'a.b.c': 1, 'x': 2)
+# d = {"a": {"b": {"c": 1}}, "x": 2}
+# flat_d = {}
+# track = [(d, "")]
+# while track:
+#     current_d, key_prefix = track.pop()
+#     for key, value in current_d.items():
+#         if key_prefix:
+#             new_key = f"{key_prefix}.{key}"
+#         else:
+#             new_key = key
+#         if isinstance(value, dict):
+#             track.append((value, new_key))
+#         else:
+#             flat_d[new_key] = value
+#
+# print(flat_d)
+
+# 5. Pascal Triangle Full Pattern:
+# Input = 5
+# Output:
+# 1
+# 1 1
+# 1 2 1
+# 1 3 3 1
+# 1 4 6 4 1
+
+# rows = 5
+# for i in range(rows):
+#     nu = 1
+#     row = []
+#     for j in range(i+1):
+#         row.append(str(nu))
+#         nu = nu * (i-j)//(j+1)
+#     print(" ".join(row))
+
+
 
 
 
